433759c40b2ede20b8a0944c4235a18d4ea45f26
ahmadullahdastagiri <ahmadullahdastagiri10@gmail.com>
2026-05-08 19:21:11 +0430

    feat: add database connection and user module structure

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,18 @@
 import express from "express";
+import dotenv from "dotenv";
 import morgan from "morgan";
+
+import connectDB from "./config/database.config.js";
+
+dotenv.config();
 
 const app = express();
 
 // middleware
 app.use(morgan("dev"));
+
+// connect to database
+connectDB();
 
 const PORT = process.env.PORT || 5000;
 
