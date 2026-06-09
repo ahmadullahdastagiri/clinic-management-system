08f78d261ba61efc7b8b0cebef493c4691ad9fbe
ahmadullahdastagiri <ahmadullahdastagiri10@gmail.com>
2026-06-09 10:10:11 +0430

    feat: enhance error handling for validation errors using Zod

--- backend/middlewares/errorHandler.middleware.js
+++ backend/middlewares/errorHandler.middleware.js
@@ -1,4 +1,5 @@
 import AppError from "../utils/AppError.js";
+import { ZodError } from "zod";
 
 export const errorHandler = (err, req, res, next) => {
   let error = err;
@@ -7,7 +8,7 @@ export const errorHandler = (err, req, res, next) => {
   const status = error.status || "error";
 
   // Log full error ONLY for non-operational or server errors
-  if (!error.isOperational) {
+  if (!error.isOperational && !(error instanceof ZodError)) {
     console.error("🔥 PROGRAMMING ERROR:", {
       message: error.message,
       stack: error.stack,
@@ -18,8 +19,30 @@ export const errorHandler = (err, req, res, next) => {
     });
   }
 
+  // validation error handling
+  if (error instanceof ZodError) {
+    const issues = error.issues.map((e) => ({
+      path: e.path.join(".") || null,
+      message: e.message,
+    }));
+    return res.status(400).json({
+      status: "fail",
+      message: "Validation error",
+      errorCode: "VALIDATION_ERROR",
+      details: issues,
+    });
+  }
+
   // Production response (safe)
   if (error.isOperational) {
+    return res.status(statusCode).json({
+      status,
+      message: error.message,
+      errorCode: error.errorCode || null,
+    });
+  }
+
+  if (error instanceof AppError) {
     return res.status(statusCode).json({
       status,
       message: error.message,
