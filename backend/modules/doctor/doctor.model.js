0c0a3a5e03f72f63f3735531bf2c28059d11b5e6
ahmadullahdastagiri <ahmadullahdastagiri10@gmail.com>
2026-06-14 11:16:02 +0430

    feat: design doctor model

---
+++ backend/modules/doctor/doctor.model.js
@@ -0,0 +1,65 @@
+import mongoose from "mongoose";
+
+const doctorSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    specialization: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    experience: {
+      type: Number,
+      required: true,
+      min: 0,
+      default: 0,
+    },
+    qualification: {
+      type: String,
+      trim: true,
+    },
+    workingHours: {
+      start: {
+        type: String,
+        required: true,
+      },
+      end: {
+        type: String,
+        required: true,
+      },
+      days: {
+        type: [String],
+        required: true,
+        enum: [
+          "Monday",
+          "Tuesday",
+          "Wednesday",
+          "Thursday",
+          "Friday",
+          "Saturday",
+          "Sunday",
+        ],
+      },
+    },
+    contactNumber: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    active: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const Doctor = mongoose.model("Doctor", doctorSchema);
+
+export default Doctor;
