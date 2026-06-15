b76205c3d32e33761277c4e7f895288c4c58a2dc
ahmadullahdastagiri <ahmadullahdastagiri10@gmail.com>
2026-06-15 09:26:11 +0430

    feat: complete repository with READ UPDATE AND DELETE functions

---
+++ backend/modules/doctor/doctor.repository.js
@@ -0,0 +1,59 @@
+import Doctor from "./doctor.model.js";
+
+const escapeRegex = (value) => value.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
+
+/* CREATE */
+export const createDoctor = (doctorData) => Doctor.create(doctorData);
+
+/* READ */
+export const getDoctorById = (id) =>
+  Doctor.findById(id)
+    .populate("userId", "firstName lastName email phone")
+    .lean();
+
+export const getAllDoctors = async ({ page = 1, limit = 10 }) => {
+  const skip = (page - 1) * limit;
+
+  const doctors = await Doctor.find()
+    .skip(skip)
+    .limit(limit)
+    .populate("userId", "firstName lastName email phone")
+    .populate("userId", "firstName lastName email phone")
+    .lean();
+
+  const totalDoctors = await Doctor.countDocuments();
+  const totalPages = Math.ceil(totalUsers / limit);
+
+  return { doctors, totalDoctors, totalPages };
+};
+
+export const findDoctor = async (key, { page = 1, limit = 10 }) => {
+  const regex = new RegExp(escapeRegex(key), "i");
+  const skip = (page - 1) * limit;
+  const doctors = await Doctor.find({
+    $or: [
+      {
+        firstName: regex,
+        lastName: regex,
+      },
+    ],
+  })
+    .skip(skip)
+    .limit(limit)
+    .populate("userId", "firstName lastName email phone")
+    .lean();
+  return doctors;
+};
+
+/* UPDATE */
+export const activateDoctorById = (id) =>
+  Doctor.findByIdAndUpdate(id, { active: true }, { new: true });
+
+export const deactivateDoctorById = (id) =>
+  Doctor.findByIdAndUpdate(id, { active: false }, { new: true });
+
+export const updateDoctorById = (id, updateData) =>
+  Doctor.findByIdAndUpdate(id, updateData, { new: true, runValidators: true });
+
+/* DELETE */
+export const deleteDoctor = (id) => Doctor.findByIdAndDelete(id);
