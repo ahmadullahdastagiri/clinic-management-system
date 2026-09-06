95cb71f6924164dbc817b668bf44932f9dba8746
ahmadullahdastagiri <ahmadullahdastagiri10@gmail.com>
2026-09-06 14:30:53 +0430

    refactor: clean up doctor schema formatting and remove unnecessary trim properties

--- backend/modules/doctor/doctor.model.js
+++ backend/modules/doctor/doctor.model.js
@@ -2,74 +2,69 @@ import mongoose from "mongoose";
 
 import generateUniqueId from "../../utils/generateUniqueId.js";
 
-const doctorSchema = new mongoose.Schema(
-  {
-    doctorCode: {
-      type: String,
-      unique: true,
-      trim: true,
-      index: true,
-    },
-    userId: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-      role: "doctor",
-      index: true,
-      required: true,
-    },
+const doctorSchema = new mongoose.Schema({
+  doctorCode: {
+    type: String,
+    unique: true,
+    trim: true,
+    index: true,
+  },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    role: "doctor",
+    index: true,
+    required: true,
+  },
 
-    specialization: {
+  specialization: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  experience: {
+    type: Number,
+    required: true,
+    min: 0,
+    default: 0,
+  },
+  qualification: {
+    type: String,
+    trim: true,
+  },
+  workingHours: {
+    start: {
       type: String,
       required: true,
-      trim: true,
     },
-    experience: {
-      type: Number,
-      required: true,
-      min: 0,
-      default: 0,
-    },
-    qualification: {
-      type: String,
-      trim: true,
-    },
-    workingHours: {
-      start: {
-        type: String,
-        required: true,
-      },
-      end: {
-        type: String,
-        required: true,
-      },
-      days: {
-        type: [String],
-        required: true,
-        enum: [
-          "Monday",
-          "Tuesday",
-          "Wednesday",
-          "Thursday",
-          "Friday",
-          "Saturday",
-          "Sunday",
-        ],
-      },
-    },
-    contactNumber: {
+    end: {
       type: String,
       required: true,
-      trim: true,
     },
-    active: {
-      type: Boolean,
-      default: true,
+    days: {
+      type: [String],
+      required: true,
+      enum: [
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+        "Sunday",
+      ],
     },
   },
-  {
-    timestamps: true,
+  contactNumber: {
+    type: String,
+    required: true,
+    trim: true,
   },
-);
+  active: {
+    type: Boolean,
+    default: true,
+  },
+});
 
 doctorSchema.pre("save", async function () {
   if (!this.isNew || this.doctorCode) {
